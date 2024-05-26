89497419b94c0ba502e327c9c887e9b4ebf0093e
Алексей Красников <aleksey.mir79@gmail.com>
2024-05-26 16:41:16 +0500

    feat: behaviors for mediatr

---
+++ src/Koshelek.Messaging.Application/Behaviors/RequestResponseLoggingBehavior.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+
+namespace Koshelek.Messaging.Application.Behaviors
+{
+    public class RequestResponseLoggingBehavior<TRequest, TResponse> :
+        IPipelineBehavior<TRequest, TResponse>
+        where TRequest : class
+        where TResponse : class
+    {
+        private readonly ILogger<RequestResponseLoggingBehavior<TRequest, TResponse>> _logger;
+
+        public RequestResponseLoggingBehavior(ILogger<RequestResponseLoggingBehavior<TRequest, TResponse>> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            var correlationId = Guid.NewGuid();
+
+            _logger.LogInformation("Handling request {CorrelationID}: {Request}",
+                correlationId,
+                JsonSerializer.Serialize(request));
+
+            var response = await next();
+
+            _logger.LogInformation("Response for {Correlation}: {Response}",
+                correlationId,
+                JsonSerializer.Serialize(response));
+
+
+            return response;
+        }
+    }
+}
